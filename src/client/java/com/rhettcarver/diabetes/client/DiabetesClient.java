ea1ddeb94b87c2fa8593dd5d096602906f99cc73
Rhett Carver <rcarver3@gatech.edu>
2025-07-29 22:47:34 -0400

    init commit

---
+++ src/client/java/com/rhettcarver/diabetes/client/DiabetesClient.java
@@ -0,0 +1,10 @@
+package com.rhettcarver.diabetes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiabetesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/rhettcarver/diabetes/Diabetes.java
@@ -0,0 +1,10 @@
+package com.rhettcarver.diabetes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Diabetes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
